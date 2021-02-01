d15344ae6dace47afcb51b5665e011b0c5c144a7
msvmsvmsv <okini-san25@mail.ru>
2021-02-01 20:55:20 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 
 public class Main {
+	//kfjdslkfjdklsfjklsd
 	public static void main(String args[]) {
 		Scanner in=new Scanner(System.in);
 		System.out.println("Input the result to find possible causes:");
